8fe67e13cc136b22108b1560e5db9f1c3e90ef49
TommyLe3825 <54484078+TommyLe3825@users.noreply.github.com>
2019-10-14 22:33:13 -0700

    Updated Constants

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -3,15 +3,15 @@
 
 int main()
 {
-  const double percent_increase = 0.18; // constant percent increase of sales
-  const long int total_sales = 2103419277; //total sales last year
+  const double PERCENT_INCREASE = 0.18; // constant percent increase of sales
+  const long int TOTAL_SALES = 2103419277; //total sales last year
   unsigned long int amount_increase; //how much more sales were there this year
   unsigned long int total_prediction; // this year's sales prediction total
 
-  amount_increase = total_sales * percent_increase; //finding how much sales are there this year
-  total_prediction = amount_increase + total_sales; //finding this year's sales prediction
+  amount_increase = TOTAL_SALES * PERCENT_INCREASE; //finding how much sales are there this year
+  total_prediction = amount_increase + TOTAL_SALES; //finding this year's sales prediction
 
-  std::cout << "Last year's sales were $" << total_sales << std::endl;
+  std::cout << "Last year's sales were $" << TOTAL_SALES << std::endl;
   std::cout << "This year's sales prediction: $"<< total_prediction;
 
   return 0;
